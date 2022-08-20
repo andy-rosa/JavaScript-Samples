f3105e868450ffca3efa7a8f120e5a2e6fbeb728
Andy Rosa <andy.roshitu@gmail.com>
2022-08-21 00:08:04 +0300

    Улучшил написание JS  кода

--- day 1/lesson/slider.js
+++ day 1/lesson/slider.js
@@ -1,15 +1,15 @@
 const slides = document.querySelectorAll('.slide');
 
-for (let slide of slides) {
-    slide.addEventListener('click', function() {
-        clearActiveClass();
-
-        this.classList.add('active');
-    })
-}
-
 function clearActiveClass() {
     slides.forEach((slide) => {
         slide.classList.remove('active');
     }) 
+}
+
+for (let slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClass();
+
+        this.classList.add('active');
+    })
 }
