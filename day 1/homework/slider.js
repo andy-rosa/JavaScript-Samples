2d523d71c91f70e35276ba313cb987355b4c3ba5
Andy Rosa <andy.roshitu@gmail.com>
2022-08-21 19:41:01 +0300

    Улучшил часть с JS

--- day 1/homework/slider.js
+++ day 1/homework/slider.js
@@ -1,26 +1,32 @@
-const slides = document.querySelectorAll('.slide'),
-backImg = [
-    'https://images.unsplash.com/photo-1615744455875-7ad33653e8c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-    'https://images.unsplash.com/photo-1561703142-b883827407ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-    'https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-    'https://images.unsplash.com/photo-1528834342297-fdefb9a5a92b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80',
-    'https://images.unsplash.com/photo-1524219726159-3cbf91ce4b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80'
-];
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide'),
+    backgrImg = [
+        'https://images.unsplash.com/photo-1615744455875-7ad33653e8c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        'https://images.unsplash.com/photo-1561703142-b883827407ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        'https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        'https://images.unsplash.com/photo-1528834342297-fdefb9a5a92b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80',
+        'https://images.unsplash.com/photo-1524219726159-3cbf91ce4b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80'
+    ];
+    
+    slides[activeSlide].classList.add('active');
 
-function clearActiveClass() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    }) 
+    function clearActiveClass() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        }) 
+    }
+    
+    for (let slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClass();
+    
+            slide.classList.add('active');
+        })
+    }
+    
+    for (let i = 0; i < slides.length; i++) {
+    
+    }
 }
 
-for (let slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClass();
-
-        slide.classList.add('active');
-    })
-}
-
-for (let i = 0; i < slides.length; i++) {
-
-}
+slidesPlugin();
